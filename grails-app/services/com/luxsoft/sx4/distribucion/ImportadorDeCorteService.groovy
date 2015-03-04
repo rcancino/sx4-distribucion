b2def9c57cb73e1afb9909502eeeacf3c6abe054
Ruben Cancino <rubencancino6@gmail.com>
2015-03-04 11:23:28 -0600

    Ajuste a importacion de corte

--- grails-app/services/com/luxsoft/sx4/distribucion/ImportadorDeCorteService.groovy
+++ grails-app/services/com/luxsoft/sx4/distribucion/ImportadorDeCorteService.groovy
@@ -14,15 +14,23 @@ class ImportadorDeCorteService {
 	def dataSource_importacion
 
 	def SQL_MESTREO="""
-    	select s.nombre as sucursal,p.clave as producto,p.descripcion,p.cantidad,p.cortes
-		,p.CORTES_INSTRUCCION as instruccion,p.CORTES_PRECIO as precioCorte,ped.folio as pedido,s.nombre
-		,v.DOCTO,ped.puesto,ped.FACTURAR,p.pedidodet_id as origen
+    	select 
+    	s.nombre as sucursal
+    	,p.clave as producto
+    	,p.descripcion
+    	,p.cantidad
+    	,p.cortes
+		,p.CORTES_INSTRUCCION as instruccion
+		,p.CORTES_PRECIO as precioCorte
+		,ped.folio as pedido
+		,s.nombre
+		,ped.puesto
+		,ped.FACTURAR
+		,p.pedidodet_id as origen
 		from sx_pedidosdet p 
 		join sx_pedidos ped on p.pedido_id=ped.pedido_id
-		join sx_ventas v on v.PEDIDO_ID=ped.PEDIDO_ID
-		join sw_sucursales s on v.SUCURSAL_ID=s.SUCURSAL_ID
+		join sw_sucursales s on p.SUCURSAL_ID=s.SUCURSAL_ID
 		where cortes>0 and  ped.pedido_id=:pedido and p.pedidodet_id=:origen
-		order by ped.creado asc
     """
 
     def importar(Surtido surtido){
@@ -31,13 +39,15 @@ class ImportadorDeCorteService {
 		def db = new Sql(dataSource_importacion)
 		surtido.partidas.each{det->
 			//log.info "Buscando instruccion de corte para $surtido.pedido $det.origen"
-			
-			db.eachRow( [pedido:surtido.origen,origen:det.origen as Integer],SQL_MESTREO) { row->
+			def select=SQL_MESTREO
+			if(surtido.forma=='TRD')
+				select="PENDIENTE"
+			db.eachRow( [pedido:surtido.origen,origen:det.origen as Integer],select) { row->
 				
 				
 				def corte=Corte.findBySurtidoDet(det)
 				if(corte){
-					log.info 'Corte ya importado'
+					log.debug 'Corte ya importado'
 				}else{
 					corte=new Corte(row.toRowResult())
 					corte.tipo=det.surtido.tipo
@@ -46,11 +56,11 @@ class ImportadorDeCorteService {
 						precioCorte=precioCorte?:0.0
 					}
 					
-					log.info "Importando instruccion  corte: "+corte.instruccion
+					log.debug "Importando instruccion  corte: "+corte.instruccion
 					det.corte=corte
 					corte.validate()
 					if(corte.hasErrors()){
-						log.info corte.errors
+						log.debug 'Errores al registrar corte: '+corte.errors+' SurtidoDet:'+det.id
 					}else{
 						det.save(flush:true,failOnError:true)
 					}
