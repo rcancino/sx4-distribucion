6de2601733edb193b51b11319b5d31954b35de76
Ruben Cancino <rubencancino6@gmail.com>
2015-07-23 08:50:56 -0600

    Ajuste a changelog

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -3,12 +3,8 @@ databaseChangeLog = {
 	changeSet(author: "rcancino (generated)", id: "changelog") {
 		// TODO add changes and preconditions here
 	}
-
 	
-
-	
-
 	include file: 'changelog_01.groovy'
 
-	//include file: 'changelog_02.groovy'
+	include file: 'changelog_02.groovy'
 }
